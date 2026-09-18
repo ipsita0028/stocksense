7d4faec7fc6ac2cdc8fbc4c20cfef7ec2b470251
ipsita0028 <ipsitasahu280@gmail.com>
2026-09-19 02:37:45 +0530

    feat: add apps and base template

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'inventory',
+    'billing',
+    'ledger',
+    'forecasting',
+    'reports',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +60,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
